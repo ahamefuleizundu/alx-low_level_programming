8dd14c1f6277eb3e395da1b8be902db2b23fdcee
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-15 14:06:10 -0700

    CP

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Program that executes a line using the Put function
- * return:0(valid)
+ * main - Program that executes a line using the puts function
+ * return:0(Succes)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
