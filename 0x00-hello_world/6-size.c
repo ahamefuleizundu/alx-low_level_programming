a0e5b481dbb9b15223d0d71a6b4dbdf06fdcb1de
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-15 18:34:09 -0700

    sc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,15 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("size of a char: %lu byte(s)\n",
+printf("size of a char: %lubyte(s)\n",
 		(unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n",
+printf("size of an int: %lubyte(s)\n",
 		(unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n",
+printf("size of a long int: %lubyte(s)\n",
 		(unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n",
+printf("size of a long long int: %lubyte(s)\n",
 		(unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n",
+printf("size of a float: %lubyte(s)\n",
 		(unsigned long)sizeof(f));
 return (0);
 }
