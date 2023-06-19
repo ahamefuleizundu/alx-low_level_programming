1b851319d57848a30ebe8abb221c50408933ed8b
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-19 12:16:18 -0700

    w

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	     for ('a' = 0; 'a' <= 10; 'a'++)            
+	          putchar(a);
+	     putchar('\n');
+	     return (0);
+}
