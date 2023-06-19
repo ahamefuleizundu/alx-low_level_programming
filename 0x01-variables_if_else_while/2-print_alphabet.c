7a76c092e50212c35f6cffee97cdd986f3b6dc7d
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-19 06:46:26 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: 0 if successful, otherwsie non 0.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
