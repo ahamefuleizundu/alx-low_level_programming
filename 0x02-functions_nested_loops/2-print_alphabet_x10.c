d73fe412845d41350d73502d2d5b6daec040711c
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-21 08:28:30 -0700

    putchar1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * main - function that prints 10 times the alphabet, in lowercase.
- * Return: void
- */
+  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * Return: Nothing.
+  */
 void print_alphabet_x10(void)
 {
+	int i;
 	char c;
-	int i = 0;
 
-	while (i <= 0)
+	for (i = 0; i < 10; ++i)
 	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	i++;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
