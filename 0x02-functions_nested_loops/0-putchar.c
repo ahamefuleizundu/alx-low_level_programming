7728cc635888ec8783bc35ab3c5ad4b030cb657d
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-20 17:34:53 -0700

    N

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,28 @@
-#include <main.h>
-/**
- * main - prints the string "_putchar" from a character array.
- * Return: 0 on success.
- */
-int main(void)
-{
-	char c[] = "_putchar";
-	int i;
+#include "main.h"
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * _putchar - writes the character c to stdout
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+int _putchar(char c)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+	return (write(1, &c, 1));
 }
+
