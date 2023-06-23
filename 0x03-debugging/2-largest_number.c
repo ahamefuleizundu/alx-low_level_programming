2785ed310cc9c48dbb52521b1dae694df18f2d8a
IZUNDU <ahamefuleizundu@gmail.com>
2023-06-22 18:50:07 -0700

    error testing

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,28 +2,33 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * first integer
- * second integer
- * third integer
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
 
-	return (largest);
+
+return (largest);
 }
