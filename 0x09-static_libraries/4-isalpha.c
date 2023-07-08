567d5f1e18561426a50e5ec143e1a949051496d1
IZUNDU <ahamefuleizundu@gmail.com>
2023-07-08 02:08:51 -0700

    libmy.a

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int _isalpha(int c)
+{
+	return (isalpha(c));
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char *_strstr(char *hystack, char *needle)
+{
+	return (strstr(hystack, needle));
+}
